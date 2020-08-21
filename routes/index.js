80fcb099d82cad91581f064754ea4bd9d8c9968f
JaronArmiger <jaron.armiger@gmail.com>
2020-08-21 15:55:22 -0500

    build form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+  	text: "what's poopin",
+  	user: "jean",
+  	added: new Date()
+  },
+  {
+  	text: "sozbaybee",
+  	user: "sosa",
+  	added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'mini message board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'New Message'});
+});
+
+router.post('/new', function(req, res, next) {
+  const formData = req.body;
+  messages.push({text: formData.messageText, 
+  	user: formData.messageUser,
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
